badeadb1912724992649f5542c6c83a92994d3a5
lzwjava <lzwjava@gmail.com>
2015-09-27 00:45:20 +0800

    finsih cube

--- Cube/Cube/ViewController.m
+++ Cube/Cube/ViewController.m
@@ -31,12 +31,38 @@
     
     CATransform3D perspective = CATransform3DIdentity;
     perspective.m34 = - 1.0 / 500.0;
-    perspective = CATransform3DRotate(perspective, -M_PI_4, 1, 0, 0);
-    perspective = CATransform3DRotate(perspective, -M_PI_4, 1, 0, 0);
+    
+    perspective = CATransform3DRotate(perspective, - M_PI_4, 1, 0, 0);
+    perspective = CATransform3DRotate(perspective, - M_PI_4, 0, 1, 0);
+    
+//    perspective = CATransform3DRotate(perspective, M_PI_4 * 5, 1, 0, 0);
+//    perspective = CATransform3DRotate(perspective, M_PI_4 * 5, 0, 1, 0);
+    
     self.containerView.layer.sublayerTransform = perspective;
     
     CATransform3D transform = CATransform3DMakeTranslation(0, 0, 100);
     [self addFace:0 withTransform:transform];
+    
+    transform = CATransform3DMakeTranslation(100, 0, 0);
+    transform = CATransform3DRotate(transform, M_PI_2, 0, 1, 0);
+    [self addFace:1 withTransform:transform];
+    
+    transform = CATransform3DMakeTranslation(0, -100, 0);
+    transform = CATransform3DRotate(transform, M_PI_2, 1, 0, 0);
+    [self addFace:2 withTransform:transform];
+    
+    transform = CATransform3DMakeTranslation(0, 100, 0);
+    transform = CATransform3DRotate(transform, - M_PI_2, 1, 0, 0);
+    [self addFace:3 withTransform:transform];
+    
+    transform = CATransform3DMakeTranslation(-100, 0, 0);
+    transform = CATransform3DRotate(transform, - M_PI_2, 0, 1, 0);
+    [self addFace:4 withTransform:transform];
+    
+    transform = CATransform3DMakeTranslation(0, 0, -100);
+    transform = CATransform3DRotate(transform, M_PI, 0, 1, 0);
+    [self addFace:5 withTransform:transform];
+    
 }
 
 - (void)didReceiveMemoryWarning {

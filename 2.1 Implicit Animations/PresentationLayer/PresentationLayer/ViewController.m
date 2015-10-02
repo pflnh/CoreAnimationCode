4de4fa09db9f4300ea1c081f41f2b30b7ae677bf
lzwjava <lzwjava@gmail.com>
2015-10-02 11:37:28 +0800

    finish PresentationLayer

--- 2.1 Implicit Animations/PresentationLayer/PresentationLayer/ViewController.m
+++ 2.1 Implicit Animations/PresentationLayer/PresentationLayer/ViewController.m
@@ -10,6 +10,9 @@
 
 @interface ViewController ()
 
+@property (strong, nonatomic) CALayer *colorLayer;
+
+
 @end
 
 @implementation ViewController
@@ -17,6 +20,29 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    self.colorLayer = [CALayer layer];
+    self.colorLayer.frame = CGRectMake(0, 0, 100, 100);
+    self.colorLayer.position = CGPointMake(CGRectGetWidth(self.view.frame) / 2, CGRectGetHeight(self.view.frame) / 2);
+    self.colorLayer.backgroundColor = [UIColor redColor].CGColor;
+    
+    [self.view.layer addSublayer:self.colorLayer];
+}
+
+- (void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event {
+    CGPoint point = [[touches anyObject] locationInView:self.view];
+    if ([self.colorLayer.presentationLayer hitTest:point]) {
+        //randomize the layer background color
+        CGFloat red = arc4random() / (CGFloat)INT_MAX;
+        CGFloat green = arc4random() / (CGFloat)INT_MAX;
+        CGFloat blue = arc4random() / (CGFloat)INT_MAX;
+        self.colorLayer.backgroundColor = [UIColor colorWithRed:red green:green blue:blue alpha:1].CGColor;
+    } else {
+        [CATransaction begin];
+        [CATransaction setAnimationDuration:4];
+        self.colorLayer.position = point;
+        [CATransaction commit];
+    }
 }
 
 - (void)didReceiveMemoryWarning {

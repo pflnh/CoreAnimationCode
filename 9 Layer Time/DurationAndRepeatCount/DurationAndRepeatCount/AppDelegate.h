ec837a7e8054859b3890d320fa70e35dbcd3da5f
lzwjava <lzwjava@gmail.com>
2015-10-02 22:46:43 +0800

    rename directory

---
+++ 9 Layer Time/DurationAndRepeatCount/DurationAndRepeatCount/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DurationAndRepeatCount
+//
+//  Created by lzw on 15/10/2.
+//  Copyright © 2015年 lzwjava. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 9 Layer Time/DurationAndRepeatCount/DurationAndRepeatCount/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DurationAndRepeatCount
+//
+//  Created by lzw on 15/10/2.
+//  Copyright © 2015年 lzwjava. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+

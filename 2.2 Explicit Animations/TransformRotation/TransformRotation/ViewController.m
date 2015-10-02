2f8254b10aa58743b6fd9f952c2b2161f9379083
lzwjava <lzwjava@gmail.com>
2015-10-02 18:41:49 +0800

    add TransformRotation

---
+++ 2.2 Explicit Animations/TransformRotation/TransformRotation/ViewController.m
@@ -0,0 +1,41 @@
+//
+//  ViewController.m
+//  TransformRotation
+//
+//  Created by lzw on 15/10/2.
+//  Copyright © 2015年 lzwjava. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@property (weak, nonatomic) IBOutlet UIView *containerView;
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+    
+    CALayer *shipLayer = [CALayer layer];
+    shipLayer.frame = CGRectMake(0, 0, 128, 128);
+    shipLayer.position = CGPointMake(150, 150);
+    shipLayer.contents = (__bridge id)[UIImage imageNamed:@"Ship"].CGImage;
+    [self.containerView.layer addSublayer:shipLayer];
+    
+    CABasicAnimation *animation = [CABasicAnimation animation];
+    animation.keyPath = @"transform.rotation";
+    animation.duration = 2;
+    animation.byValue = @(M_PI * 2);
+    [shipLayer addAnimation:animation forKey:nil];
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
